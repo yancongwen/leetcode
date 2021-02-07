cab439d84bcbad48a4942909186e6a2d5aa46de8
yancongwen <ycwen1119@163.com>
2021-02-07 15:59:06 +0800

    feat: 反转链表

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 - [134. 加油站](https://github.com/yancongwen/leetcode/blob/master/src/134-gas-station.js)
 - [141. 环形链表](https://github.com/yancongwen/leetcode/blob/master/src/141-linked-list-cycle.js)
+- [206. 反转链表](https://github.com/yancongwen/leetcode/blob/master/src/206-reverse-linked-list.js)
 - [316. 去除重复字母](https://github.com/yancongwen/leetcode/blob/master/src/316-remove-duplicate-letters.js)
---
+++ src/206-reverse-linked-list.js
@@ -0,0 +1,45 @@
+/*
+ * @leetcode: https://leetcode-cn.com/problems/reverse-linked-list/
+ * @Descripttion: 反转链表
+ * @Author: ycwen1119@163.com
+ * @Date: 2021-02-07 14:17:39
+ */
+
+ /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @description 方法一：迭代法，从前向后处理
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null
+    while(head) {
+        let next = head.next
+        head.next = prev
+        prev = head
+        head = next
+    }
+    return prev
+};
+
+/**
+ * @description 方法二：递归，从后向前处理
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (!head || !head.next) {
+        return head
+    }
+    let newHead = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    return newHead
+};
