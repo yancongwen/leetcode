15393ffaaeef57ada745bdce0005b82219bef402
yancongwen <ycwen1119@163.com>
2021-03-05 09:18:11 +0800

    upd

--- src/46-permutations.js
+++ src/46-permutations.js
@@ -11,21 +11,21 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-// var permute = function(nums) {
-//     let len = nums.length
-//     let result = []
-//     if (len <= 1) {
-//         return nums
-//     }
-//     for (let i = 0; i < len; i++) {
-//         let otherNums = [...nums.slice(0, i), ...nums.slice(i + 1)]
-//         let temp = permute(otherNums)
-//         temp.forEach(item => {
-//             result.push([nums[i], ...item])
-//         })
-//     }
-//     return result
-// }
+var permute = function(nums) {
+    let len = nums.length
+    let result = []
+    if (len <= 1) {
+        return nums
+    }
+    for (let i = 0; i < len; i++) {
+        let otherNums = [...nums.slice(0, i), ...nums.slice(i + 1)]
+        let temp = permute(otherNums)
+        temp.forEach(item => {
+            result.push([nums[i], ...item])
+        })
+    }
+    return result
+}
 
 /**
  * @Description: 方法二：回溯 + 深度优先遍历 + 递归
@@ -33,12 +33,13 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
+    let len = nums.length
     let res = []
-    if (nums.length === 0) {
+    if (len === 0) {
         return res
     }
-    let used = {} // 用于存储是否已存在于 path
-    let path = [] // 遍历路径
+    let path = [] // 当前路径
+    let used = {} // 存储是否已存在于 path
     dfs(nums, path, used, res)
     return res
 }
@@ -54,12 +55,47 @@ var dfs = function(nums, path, used, res) {
         if (!used[i]) {
             path.push(nums[i])
             used[i] = true
-            // 递归
+            // 进入下一层
             dfs(nums, path, used, res)
+            // 回溯
             path.pop()
             used[i] = false
         }
     }
 }
 
-console.log(permute([1,2,3]))
+/**
+ * @Description: 方法二的另一种形式
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let len = nums.length
+    let res = []
+    if (len === 0) {
+        return res
+    }
+    let path = [] // 当前路径
+    let used = {} // 存储是否已存在于 path
+    let dfs = function() {
+        // console.log(path)
+        if (path.length === len) {
+            res.push([...path])
+        }
+        for (let i = 0; i < len; i++) {
+            if (!used[i]) {
+                used[i] = true
+                path.push(nums[i])
+                // 进入下一层
+                dfs()
+                // 回溯
+                used[i] = false
+                path.pop()
+            }
+        }
+    }
+    dfs(0)
+    return res
+}
+
+// console.log(permute([1,2,3]))
